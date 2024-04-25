70667c50458bd0904bd4f867067097e1aad8bdd2
thor1878 <ttma18@student.aau.dk>
2024-04-25 09:40:11 +0200

    init stuff

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500">TES</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Game/Game.tsx
@@ -0,0 +1,20 @@
+import { P5CanvasInstance, ReactP5Wrapper } from '@p5-wrapper/react';
+
+function sketch(p5: P5CanvasInstance) {
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+  p5.draw = () => {
+    p5.background(250);
+    p5.normalMaterial();
+    p5.push();
+    p5.rotateZ(p5.frameCount * 0.01);
+    p5.rotateX(p5.frameCount * 0.01);
+    p5.rotateY(p5.frameCount * 0.01);
+    p5.plane(100);
+    p5.pop();
+  };
+}
+
+export const Game = () => {
+  return <ReactP5Wrapper sketch={sketch} />;
+};
